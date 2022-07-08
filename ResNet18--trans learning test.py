97bfa0678f4eab32ea132cc9ec9f22ce06beb157
Chen Shengbang <88441693+FilippoChen09@users.noreply.github.com>
2022-07-09 02:33:33 +0800

    Update ResNet18--trans learning test.py

--- ResNet18--trans learning test.py
+++ ResNet18--trans learning test.py
@@ -3,10 +3,14 @@ import torch
 import matplotlib.pyplot as plt
 import cv2
 
+
 # 设置超参数
 filepath = 'milan girl.jpg'
-image_height = 523
-image_width = 527
+image = plt.imread(filepath)
+image_height = image.shape[0]
+image_width = image.shape[1]
+image_channel = image.shape[2]
+num_batch = 1
 
 model = torchvision.models.resnet18(pretrained=True)
 # 实例化resnet18对象
@@ -17,10 +21,9 @@ totensor = torchvision.transforms.ToTensor()
 # print(model)
 # 可以打印出网络的结构
 
-image = plt.imread(filepath)
 input_image = totensor((cv2.resize(image,(image_width,image_height))))
 # cv2.resize(img,(width,height))此方法中图片宽在前，高在后
-input_image = input_image.reshape(1,3,523,527)
+input_image = input_image.reshape(num_batch,image_channel,image_height,image_width)
 # 读取、resize二进制文件，并转换为torch的张量
 
 output = model(input_image)
